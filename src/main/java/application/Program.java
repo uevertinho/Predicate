8ab67cbfc3e79250b407bc9a39c3e1eaa3dda459
uevertinho <devvertinho@gmail.com>
2025-10-30 17:16:58 -0300

    Expressão lambda declarada

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -6,10 +6,13 @@ import util.ProductPredicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         List<Product> list = new ArrayList<>();
         list.add(new Product("Tv", 900.00));
@@ -17,10 +20,13 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        // method reference:
-        // colocamos o nome da classe onde o metodo está '::' e aí nome do metodo
-        // isso aqui também é aceito no sistema lambda do java
-        list.removeIf(Product::nonstaticProductPredicate);
+        //desse jeito é legal porque podemos pegar valor de variáveis
+        //e parametrizarmos esse valor.
+        //inclusive as que o usuário digitar e parametrizar na expressão
+        double min = sc.nextDouble();
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
 
         for (Product p : list){
             System.out.println(p);
